8b5411106c83e6990852ee175b03aa3c2ebb8d73
Callum Bromley <callumbromleydev@gmail.com>
2025-03-27 18:37:59 +1300

    Completed full contact and added words to left

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -2,32 +2,34 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    
     <section className="contact">
-      <form>
-        
-        <div className="input-box">
-          <label></label>
-          <input type="text" className="field" placeholder="Name" required />
+      <div className="contact-container">
+        <div className="contact-text">
+          <h1>Auckland’s Roofing Experts</h1>
+          <p>
+            Aspect Roofing offers decades of combined experience in the roofing indus
+          </p>
         </div>
-        <div className="input-box">
-          <label></label>
-          <input type="email" className="field" placeholder="Email" required />
-        </div>
-        <div className="input-box">
-          <label></label>
-          <input type="text" className="field" placeholder="Phone" required />
-        </div>
-        <div className="input-box">
-          <label></label>
-          <input type="text" className="field" placeholder="Address" required />
-        </div>
-        <div className="input-box">
-          <label></label>
-          <textarea className="field mess" placeholder="Enter your message" required />
-        </div>
-        <button type="submit">Send Message</button>
-      </form>
+
+        <form>
+          <div className="input-box">
+            <input type="text" className="field" placeholder="Name" required />
+          </div>
+          <div className="input-box">
+            <input type="email" className="field" placeholder="Email" required />
+          </div>
+          <div className="input-box">
+            <input type="text" className="field" placeholder="Phone" required />
+          </div>
+          <div className="input-box">
+            <input type="text" className="field" placeholder="Address" required />
+          </div>
+          <div className="input-box">
+            <textarea className="field mess" placeholder="Enter your message" required />
+          </div>
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
     </section>
   );
 };
